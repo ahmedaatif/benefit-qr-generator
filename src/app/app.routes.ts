af356a83da03514809e280032f3d18bcd5e650ba
Ahmed A'atif <ahmedaatif1999@gmail.com>
2024-12-28 14:46:58 +0300

    website base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GenerateQrComponent } from './pages/generate-qr/generate-qr.component';
+import { NotFoundComponent } from './pages/util/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        component: AboutComponent
+    },
+    {
+        path: 'generate-qr',
+        component: GenerateQrComponent
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+    {
+        path: '**',
+        component: NotFoundComponent
+    }
+];
---
+++ src/app/pages/generate-qr/generate-qr.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-qr',
+  imports: [],
+  templateUrl: './generate-qr.component.html',
+  styleUrl: './generate-qr.component.scss'
+})
+export class GenerateQrComponent {
+
+}
